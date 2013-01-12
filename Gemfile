070dfcfa2b475ef7e7a2d565480ea3c93cce71ef
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-11 20:15:53 -0600

    rack-cache

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source :rubygems
 gem "capistrano"
 gem "haml"
 gem "sinatra"
+gem "rack-cache"
 
 group :production do
   gem 'thin'
--- application.rb
+++ application.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'rack/cache'
 require_relative 'environment'
+
+use Rack::Cache do
+  set :metastore, 'heap:/'
+  set :entitystore, 'heap:/'
+end
 
 class TexAppOrg < Sinatra::Base
   set :haml, :format => :html5
